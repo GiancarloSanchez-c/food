c838a3fa34e465e2bb1a924d9a9253c32273f02d
GiancarloSanchez-c <gian_23080@hotmail.com>
2022-06-28 13:07:24 -0500

    make it better

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,14 +27,17 @@ const Navbar = () => {
             <div className="container-header-txt">
               <div className="header-txt">
                 <h1>The best recipes</h1>
-                <h1>and <br/> the best food</h1>
+                <h1>and <br /> the best food</h1>
               </div>
             </div>
             <div className="topnav">
               <a href="#" onClick={handleClick}>Home</a>
               <Link to={'/create'}>Create Recipe</Link>
-              <Search />
+              <div>
+                <Search />
+              </div>
             </div>
+
           </div>
         </div>
       </header>
